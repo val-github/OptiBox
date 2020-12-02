3332206d42a8f69ef164926514148a1ed0b9b846
val <z305blet@gmail.com>
2020-12-02 11:45:05 +0100

    modifDB

--- OptiBox2i/src/test/java/modifDB.java
+++ OptiBox2i/src/test/java/modifDB.java
@@ -1,3 +1,9 @@
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +15,33 @@
  * @author Val
  */
 public class modifDB {
-    
+    public static void main(String[]args) {
+    final EntityManagerFactory emf=Persistence.createEntityManagerFactory("OptiBox");
+    final EntityManager em=emf.createEntityManager();
+    try{
+        final EntityTransaction et=em.getTransaction();
+        try{
+        // creation d’une entite persistante*
+        et.begin();
+        Service serv1=new Service("bouffe","Bat A, 1er étage");
+        Service serv2=new Service("test2","Bat B, 1er étage");
+        Service serv3=new Service("cardio","Bat C, 1er étage");
+        
+        em.persist(serv1);
+        em.persist(serv2);
+        em.persist(serv3);
+        serv1.setLocalisation("Bat D, 2ème étage");
+        et.commit();
+    }catch(Exception ex) {
+        et.rollback();
+    }
+    }finally{
+        if(em!=null&&em.isOpen()){
+            em.close();
+        }
+        if(emf!=null&&emf.isOpen()){
+            emf.close();
+        }
+    }
+}
 }
