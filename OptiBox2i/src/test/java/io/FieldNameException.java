ca98103d5f5adcd55c6678a2f0aaad43d5dde26b
val <z305blet@gmail.com>
2020-12-08 17:10:25 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ OptiBox2i/src/test/java/io/FieldNameException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io;
+
+/**
+ *
+ * @author Val
+ */
+public class FieldNameException extends ReaderException {
+    public FieldNameException (int numLigne, int numColonne, String val) {
+        super("La ligne numéro "+numLigne+" du fichier est incorrecte",
+                "Assurez-vous que ligne numéro "+numLigne+" contienne en colonne "+numColonne+" la chaîne de caractères '"+val+"'");
+    }
+}
+
